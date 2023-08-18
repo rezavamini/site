da049bdabcd5be61226c7c1813ddcf24192fe264
rezavamini <139910634+rezavamini@users.noreply.github.com>
2023-08-18 19:44:35 +0330

    fix some bugs

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,8 @@ from blog.models import Post
 class postadmin(admin.ModelAdmin):
  date_hierarchy = "created_date"
  empty_value_display = "-empty-"  
- list_display = ("title",'created_date',"counted_view","status","published_date")
- list_filter = ["status"]
+ list_display = ("title",'author','created_date',"counted_view","status","published_date")
+ list_filter = ["status",'author']
  search_fields = ['title','content']
  
 
--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import get_object_or_404, render
 from blog.models import Post
 from django.utils import timezone
 
-
-def sum_counted_view(pid):
-    post = Post.objects.get(id=pid)
-    post.counted_view += 1
-    post.save()
-    
-    
-    
 def blog_view(request):
     corrent_time = timezone.now()
     Posts = Post.objects.filter(published_date__lte=corrent_time,status = 1)
@@ -17,8 +9,13 @@ def blog_view(request):
     return render(request,"blog/blog-home.html",context)
 
 def blog_single(request,pid):
-    sum_counted_view(pid)
-    post = get_object_or_404(Post,pk=pid)
+    #جمع کردن ویو 
+    post = Post.objects.get(id=pid)
+    post.counted_view += 1
+    post.save()
+    #
+    corrent_time = timezone.now()
+    post = get_object_or_404(Post,pk=pid,published_date__lte=corrent_time,status = 1)
     context = {'post':post}
     return render(request,"blog/blog-single.html",context)
 
