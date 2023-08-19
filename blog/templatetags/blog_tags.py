65714d335a4fde11cb3a6c1a4de170087eb33ac6
rezavamini <139910634+rezavamini@users.noreply.github.com>
2023-08-19 20:40:04 +0330

    add post category side bar

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from blog.models import Category
 
 register = template.Library()
 
@@ -8,3 +9,11 @@ def latestposts(arg=4):
     posts = Post.objects.filter(status = 1).order_by('-published_date')[:arg]
     return {'posts': posts}
         
+@register.inclusion_tag('blog/blog-category.html')        
+def postcategories():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category = name).count()
+    return {'categories':cat_dict}
